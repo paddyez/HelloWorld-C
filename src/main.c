d93324bc03f196cfa36002045a0c93eeb9ef9945
Patrick-Emil Zörner <paddyez@gmail.com>
2020-02-03 18:22:25 +0100

    Flags; args

--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <main.h>
 
-int main(void) {
+int main(int argc, char *argv[]) {
+	if (argc < 3) {
+		fprintf(stderr, "usage %s param1 param2\n", argv[0]);
+  		exit(0);
+	}
 	print_hello_world();
-	return(EXIT_SUCCESS);
+	return EXIT_SUCCESS;
 }
