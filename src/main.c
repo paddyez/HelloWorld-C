a23648fe90a62cb83bb6b96c574639e5345520fa
Patrick-Emil Zörner <paddyez@gmail.com>
2020-02-05 12:18:47 +0100

    Input checks

--- src/main.c
+++ src/main.c
@@ -1,19 +1,39 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <main.h>
 
 int main(int argc, char *argv[]) {
-	if (argc < 4) {
-		fprintf(stderr, "usage: %s <celsius start> <celsius stop> <increment>\n", argv[0]);
+	if (argc != 4) {
+		fprintf(stderr, "usage: %s <start> <stop> <increment>\n", argv[0]);
   		exit(0);
 	}
 	/*
 	print_hello_world();
 	*/
-	float start = (float)atof(argv[1]);
-	float stop = (float)atof(argv[2]);
-	float increment = (float)atof(argv[3]);
-	printf("arguments: %d %6.2f\t%6.2f\t%6.2f\n", argc, start, stop, increment);
-	create_celsius_conversion_table(start, stop, increment);
+	errno = 0;
+	char *endptrstart, *endptrstop, *endptrincrement;
+	float start = strtof(argv[1], &endptrstart);
+	float stop = strtof(argv[2], &endptrstop);
+	float increment = strtof(argv[3], &endptrincrement);
+	if((*endptrstart != '\0' || *endptrstop != '\0' || *endptrincrement != '\0') || errno != 0) {
+		if(errno == 0) {
+			errno = EINVAL;
+		}
+		error("Error parsing arguments expected three numbers of type float");
+	} else if (start > stop) {
+		error("stop must be smaller than start");
+	} else if (increment <= 0) {
+		error("increment must be greater than 0");
+	} else {
+		printf("start: %6.2f\tstop: \t%6.2f\tincrement: \t%6.2f\n", start, stop, increment);
+		create_celsius_conversion_table(start, stop, increment);
+		create_fahrenheit_conversion_table(start, stop, increment);
+	}
 	return EXIT_SUCCESS;
+}
+
+void error(const char *msg) {
+	perror(msg);
+	exit(EXIT_FAILURE);
 }
